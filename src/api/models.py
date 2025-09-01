0fe378194b38784b727f09cc05832eb1329c0137
JeffreyM12 <62064110+JeffreyM12@users.noreply.github.com>
2025-09-01 15:47:59 +0000

    user model

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,24 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey, Integer, DateTime, Text
+from flask import Flask
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from datetime import datetime
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    # Also known as the child model
 
+    __tablename__ = "User"
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+    user_id: Mapped[int] = mapped_column(primary_key=True)
+    display_name: Mapped[str] = mapped_column(String(120), nullable=False)
+    parent_email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)  # COPPA compliance
+    google_id: Mapped[str] = mapped_column(String(120), nullable=False)
+
+    created_at: Mapped[datetime] = mapped_column(DateTime())
+    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=True)
+    is_verified: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    avatar_selection: Mapped[str] = mapped_column(String(200), nullable=True)
