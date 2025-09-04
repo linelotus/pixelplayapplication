6d3b84f972111f0a98e56a2994f00a76b6adb7b5
JeffreyM12 <62064110+JeffreyM12@users.noreply.github.com>
2025-09-04 15:05:04 +0000

    added reward/userreward models

--- src/api/models.py
+++ src/api/models.py
@@ -17,8 +17,40 @@ class User(db.Model):
     parent_email: Mapped[str] = mapped_column(
         String(120), unique=True, nullable=False)  # COPPA compliance
     google_id: Mapped[str] = mapped_column(String(120), nullable=False)
-
     created_at: Mapped[datetime] = mapped_column(DateTime())
+    points: Mapped[int] = mapped_column(Integer, default=0) # points field for the user
+
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=True)
     is_verified: Mapped[bool] = mapped_column(Boolean(), nullable=False)
     avatar_selection: Mapped[str] = mapped_column(String(200), nullable=True)
+
+    rewards: Mapped[list["Reward"]] = relationship(
+        "Reward", back_populates="user")
+
+
+class Reward(db.Model):
+
+    __tablename__ = "Reward"
+
+    reward_id: Mapped[int] = mapped_column(primary_key=True)
+
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("User.user_id"), nullable=False)
+
+    reward_name: Mapped[str] = mapped_column(String(100), nullable=False)
+    points: Mapped[int] = mapped_column(Integer, nullable=False)
+    category: Mapped[str] = mapped_column(String(50), nullable=False)
+    description: Mapped[str] = mapped_column(Text, nullable=True)
+    is_available: Mapped[bool] = mapped_column(Boolean(), nullable=True)
+
+    user: Mapped[list["User"]] = relationship("User", back_populates="rewards")
+
+class UserReward(db.Model):
+
+    __tablename__ = "UserReward"
+
+    user_reward_id: Mapped[int] = mapped_column(primary_key=True)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("User.user_id"), nullable=False)
+    reward_id: Mapped[int] = mapped_column(ForeignKey("Reward.reward_id"), nullable=False)
+    purchased_at:  Mapped[datetime] = mapped_column(DateTime())
